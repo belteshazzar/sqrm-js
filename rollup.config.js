24982ebfd029fc3128a35835a472feb0f2faa00f
Daniel Walton <daniel@belteshazzar.com.au>
2022-11-01 21:00:29 +1030

    added npm version to bundle via rollup

--- rollup.config.js
+++ rollup.config.js
@@ -3,16 +3,17 @@ import json from '@rollup/plugin-json';
 import { terser } from 'rollup-plugin-terser';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import pkg from './package.json'
 
 export default {
   input: 'src/sqrm.js',
   output: [{
-    file: 'build/sqrm.js',
+    file: `build/sqrm-${pkg.version}.js`,
     name: 'sqrm',
     format: 'es',
     sourcemap: true
   }, {
-    file: 'build/sqrm.min.js',
+    file: `build/sqrm-${pkg.version}.min.js`,
     format: 'es',
     name: 'sqrm',
     plugins: [terser()],
